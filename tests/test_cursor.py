d6477cf6d7f79f6734bf5d2240933181aa037df2
Schamper <1254028+Schamper@users.noreply.github.com>
2024-05-16 10:05:21 +0200

    Add additional search test

--- tests/test_cursor.py
+++ tests/test_cursor.py
@@ -45,3 +45,15 @@ def test_cursor_search(ual_db: BinaryIO) -> None:
     )
 
     assert len(records) == 5
+
+    cursor.reset()
+    records = list(
+        cursor.find_all(
+            AuthenticatedUserName="blackclover\\administrator",
+            RoleGuid="ad495fc3-0eaa-413d-ba7d-8b13fa7ec598",
+            TenantId="2417e4c3-5467-40c5-809b-12b59a86c102",
+            Day204=4,
+        )
+    )
+
+    assert len(records) == 1
